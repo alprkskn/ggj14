e6397785400771c93b284089054a05e73d6f4b24
Alper Keskin <alprkskn@gmail.com>
2014-01-26 14:34:40 +0200

    fixed tp points

--- Assets/Scripts/Objects/InteractObject.cs
+++ Assets/Scripts/Objects/InteractObject.cs
@@ -6,13 +6,13 @@ public class InteractObject : MonoBehaviour {
 	public player.DrugEnums[] contains;
 	private bool empty = false;
 
-	public Vector3 tpPoint1, tpPoint2;
+	public Transform tpPoint1, tpPoint2;
 
 	public void teleportPlayer(Transform plyr) {
-		float d1 = Vector3.Distance(plyr.position, tpPoint1);
-		float d2 = Vector3.Distance(plyr.position, tpPoint2);
+		float d1 = Vector3.Distance(plyr.position, tpPoint1.position);
+		float d2 = Vector3.Distance(plyr.position, tpPoint2.position);
 
-		plyr.position = (d1 < d2) ? tpPoint2 : tpPoint1;
+		plyr.position = (d1 < d2) ? tpPoint2.position : tpPoint1.position;
 	}
 
 	public List<player.DrugEnums> getItems() {
